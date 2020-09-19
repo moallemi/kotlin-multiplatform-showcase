d9f4d4c26e187b86d102c1edd9e0e7eb7da4b0f2
Reza Moallemi <rm.allmails@gmail.com>
2020-09-19 16:48:58 +0200

    [SHARED] Add AppListViewModel

--- shared/src/commonMain/kotlin/me/moallemi/kmmshowcase/shared/di/domain.kt
+++ shared/src/commonMain/kotlin/me/moallemi/kmmshowcase/shared/di/domain.kt
@@ -8,16 +8,26 @@ import me.moallemi.kmmshowcase.shared.domain.model.Links
 import me.moallemi.kmmshowcase.shared.network.response.AppDto
 import me.moallemi.kmmshowcase.shared.network.response.LinksDto
 import me.moallemi.kmmshowcase.shared.repository.AppRepository
-import org.koin.core.context.loadKoinModules
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 internal val domainModule = module {
-    single<Mapper<LinksDto?, Links?>>(named("a")) {
+    single<Mapper<LinksDto?, Links?>>(
+        named(QUALIFIER_NAME_MAPPER_LINKSDTO_TOLINKS)
+    ) {
         LinksDtoToLinks()
     }
-    single<Mapper<AppDto, App>>(named("b")) {
-        AppDtoToApp(get(named("a")))
+    single<Mapper<AppDto, App>>(
+        named(QUALIFIER_NAME_MAPPER_APPDTO_TO_APP)
+    ) {
+        AppDtoToApp(
+            get(named(QUALIFIER_NAME_MAPPER_LINKSDTO_TOLINKS))
+        )
     }
-    single { AppRepository(get(), get(named("b"))) }
+    single {
+        AppRepository(
+            get(),
+            get(named(QUALIFIER_NAME_MAPPER_APPDTO_TO_APP))
+        )
+    }
 }
---
+++ shared/src/commonMain/kotlin/me/moallemi/kmmshowcase/shared/di/qualifires.kt
@@ -0,0 +1,4 @@
+package me.moallemi.kmmshowcase.shared.di
+
+internal const val QUALIFIER_NAME_MAPPER_LINKSDTO_TOLINKS = "qn_mapper_linksdto_tolinks"
+internal const val QUALIFIER_NAME_MAPPER_APPDTO_TO_APP = "qn_mapper_appdto_to_app"
