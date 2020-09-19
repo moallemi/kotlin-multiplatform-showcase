afa3490318636e1ffce60776c2eb389b2bf07520
Saeed Taheri <saeed.taheri@gmail.com>
2020-09-20 01:49:52 +0430

    [iOS]: Show apps list in a basic manner.

---
+++ iosApp/KmpShowcase/AppListViewModelWrapper.swift
@@ -0,0 +1,46 @@
+//
+//  AppListViewModelWrapper.swift
+//  KmpShowcase
+//
+//  Created by Saeed Taheri on 9/20/20.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import Combine
+import shared
+
+final class AppListViewModelWrapper: ObservableObject {
+
+	@Published private(set) var apps = [App]()
+
+	private let viewModel: AppListViewModel
+
+	init() {
+		viewModel = koin.get(objCClass: AppListViewModel.self, qualifier: nil) as! AppListViewModel
+		viewModel.apps.collect(collector: self) { _, _ in }
+	}
+
+	func load() {
+		viewModel.load()
+	}
+}
+
+extension AppListViewModelWrapper: Kotlinx_coroutines_coreFlowCollector {
+	public func emit(value: Any?, completionHandler: @escaping (KotlinUnit?, Error?) -> Void) {
+		defer {
+			completionHandler(nil, nil)
+		}
+
+		guard let items = value as? [App] else {
+			return
+		}
+
+		apps = items
+	}
+}
+
+extension App: Identifiable {
+	public var id: Int {
+		return self.hashValue
+	}
+}
--- iosApp/KmpShowcase/ContentView.swift
+++ iosApp/KmpShowcase/ContentView.swift
@@ -1,14 +1,62 @@
 import SwiftUI
 import shared
 
-func greet() -> String {
-    return Greeting().greeting()
-}
-
 struct ContentView: View {
+	@ObservedObject private var viewModel = AppListViewModelWrapper()
+
     var body: some View {
-        Text(greet())
+		NavigationView {
+			List(viewModel.apps) { app in
+				VStack(alignment: .leading) {
+					Text(app.name)
+						.font(.headline)
+						.fontWeight(.bold)
+					Text(app.description)
+						.font(.body)
+
+					makeLinksButtons(using: app.links)
+				}
+			}
+			.navigationBarTitle("KMP Showcase")
+			.onAppear {
+				viewModel.load()
+			}
+		}
     }
+
+	@ViewBuilder
+	private func makeLinksButtons(using links: Links?) -> some View {
+		if let links = links {
+			ScrollView(.horizontal, showsIndicators: false) {
+				HStack {
+					links.appStore.map { link in
+						Button("App Store") {
+							didClickOn(link)
+						}
+					}
+					links.googlePlay.map { link in
+						Button("Google Play") {
+							didClickOn(link)
+						}
+					}
+					links.website.map { link in
+						Button("Website") {
+							didClickOn(link)
+						}
+					}
+				}
+			}
+		} else {
+			EmptyView()
+		}
+	}
+
+	private func didClickOn(_ link: String) {
+		guard let url = URL(string: link) else {
+			return
+		}
+		UIApplication.shared.open(url, options: [:], completionHandler: nil)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
