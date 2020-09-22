c956076b8db081cb131a0bf954a8662ac49c99f2
Reza Moallemi <rm.allmails@gmail.com>
2020-09-22 18:11:13 +0200

    [AND] Enable minifying release build

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,7 +47,9 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
