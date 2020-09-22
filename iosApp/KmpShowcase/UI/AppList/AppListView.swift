4106e31ae4ff68b557a818033622fb2ae4107c67
Saeed Taheri <saeed.taheri@gmail.com>
2020-09-22 12:42:35 +0330

    [iOS]: Refactor AppListRow and add ProgressView.

--- iosApp/KmpShowcase/UI/AppList/AppListView.swift
+++ iosApp/KmpShowcase/UI/AppList/AppListView.swift
@@ -1,97 +1,34 @@
 import SwiftUI
-import FetchImage
-import shared
 
 struct AppListView: View {
 	@StateObject private var viewModel = AppListViewModelWrapper()
 
 	var body: some View {
 		NavigationView {
-			List {
-				Image("kmp-artwork")
-					.resizable()
-					.aspectRatio(contentMode: .fit)
-					.padding()
-					.frame(height: 200)
-					.frame(maxWidth: .infinity)
-					.background(Color.accentColor)
-					.listRowInsets(EdgeInsets())
-				ForEach(viewModel.apps) { app in
-					VStack(alignment: .leading, spacing: 8) {
-						if let urlStr = app.bannerUrl,
-						   let url = URL(string: urlStr) {
-							ImageView(
-								image: FetchImage(url: url),
-								maxWidth: 300)
-								.frame(height: 150)
-								.clipped()
-								.padding(.horizontal)
-						}
-
-						Text(app.name)
-							.font(.title2)
-							.fontWeight(.semibold)
-							.padding(.horizontal)
-						Text(app.summary)
-							.foregroundColor(.secondary)
-							.font(.body)
-							.padding(.horizontal)
-
-						makeLinksButtons(using: app.links)
+			if viewModel.apps.isEmpty {
+				ProgressView("Loading…")
+			} else {
+				List {
+					Image("kmp-artwork")
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.padding()
+						.frame(height: 200)
+						.frame(maxWidth: .infinity)
+						.background(Color.accentColor)
+						.listRowInsets(EdgeInsets())
+					ForEach(viewModel.apps) { app in
+						AppRowView(app: app)
+							.listRowInsets(EdgeInsets())
 					}
-					.listRowInsets(EdgeInsets())
+					.padding(.vertical)
 				}
-				.padding(.vertical)
-			}
-			.listStyle(InsetGroupedListStyle())
-			.navigationBarTitle("KMP Showcase")
-			.onAppear {
-				viewModel.load()
+				.listStyle(InsetGroupedListStyle())
+				.navigationBarTitle("KMP Showcase")
 			}
 		}
+		.onAppear(perform: viewModel.load)
 		.navigationViewStyle(StackNavigationViewStyle())
-	}
-
-	@ViewBuilder
-	private func makeLinksButtons(using links: Links?) -> some View {
-		if let links = links {
-			ScrollView(.horizontal, showsIndicators: false) {
-				HStack(spacing: 8) {
-					links.appStore.map { link in
-						Button(action: {
-							didClickOn(link)
-						}) {
-							Label("App Store", image: "appstore")
-						}
-					}
-					links.googlePlay.map { link in
-						Button(action: {
-							didClickOn(link)
-						}) {
-							Label("Google Play", systemImage: "g.circle")
-						}
-					}
-					links.website.map { link in
-						Button(action: {
-							didClickOn(link)
-						}) {
-							Label("Website", systemImage: "globe")
-						}
-					}
-				}
-				.padding([.horizontal, .top])
-				.buttonStyle(ChipButtonStyle())
-			}
-		} else {
-			EmptyView()
-		}
-	}
-
-	private func didClickOn(_ link: String) {
-		guard let url = URL(string: link) else {
-			return
-		}
-		UIApplication.shared.open(url, options: [:], completionHandler: nil)
 	}
 }
 
---
+++ iosApp/KmpShowcase/UI/AppList/AppRowView.swift
@@ -0,0 +1,97 @@
+//
+//  AppRowView.swift
+//  KmpShowcase
+//
+//  Created by Saeed Taheri on 9/22/20.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import SwiftUI
+import FetchImage
+import shared
+
+struct AppRowView: View {
+	let app: shared.App
+
+	var body: some View {
+		VStack(alignment: .leading, spacing: 8) {
+			if let urlStr = app.bannerUrl,
+			   let url = URL(string: urlStr) {
+				ImageView(
+					image: FetchImage(url: url),
+					maxWidth: 300)
+					.frame(height: 150)
+					.clipped()
+					.padding(.horizontal)
+			}
+
+			Text(app.name)
+				.font(.title2)
+				.fontWeight(.semibold)
+				.padding(.horizontal)
+			Text(app.summary)
+				.foregroundColor(.secondary)
+				.font(.body)
+				.padding(.horizontal)
+
+			makeLinksButtons(using: app.links)
+		}
+	}
+
+	@ViewBuilder
+	private func makeLinksButtons(using links: Links?) -> some View {
+		if let links = links {
+			ScrollView(.horizontal, showsIndicators: false) {
+				HStack(spacing: 8) {
+					links.appStore.map { link in
+						Button(action: {
+							didClickOn(link)
+						}) {
+							Label("App Store", image: "appstore")
+						}
+					}
+					links.googlePlay.map { link in
+						Button(action: {
+							didClickOn(link)
+						}) {
+							Label("Google Play", systemImage: "g.circle")
+						}
+					}
+					links.website.map { link in
+						Button(action: {
+							didClickOn(link)
+						}) {
+							Label("Website", systemImage: "globe")
+						}
+					}
+				}
+				.padding([.horizontal, .top])
+				.buttonStyle(ChipButtonStyle())
+			}
+		} else {
+			EmptyView()
+		}
+	}
+
+	private func didClickOn(_ link: String) {
+		guard let url = URL(string: link) else {
+			return
+		}
+		UIApplication.shared.open(url, options: [:], completionHandler: nil)
+	}
+}
+
+struct AppRowView_Previews: PreviewProvider {
+    static var previews: some View {
+		AppRowView(
+			app: shared.App(
+				id: UUID().uuidString,
+				name: "Golchin",
+				summary: "Golchin is the best app possible",
+				links: Links(appStore: "",
+							 googlePlay: "",
+							 website: ""),
+				bannerUrl: "")
+		)
+    }
+}
