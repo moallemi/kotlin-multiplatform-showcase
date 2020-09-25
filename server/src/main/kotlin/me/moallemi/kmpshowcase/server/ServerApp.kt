f29e0bfa11fad41ea6ea8fdab99f30a8aead640d
Farzad Tabashir <farzad.tabashir@balad.ir>
2020-09-25 16:47:58 +0330

    remove redundant cors installation

--- server/src/main/kotlin/me/moallemi/kmpshowcase/server/ServerApp.kt
+++ server/src/main/kotlin/me/moallemi/kmpshowcase/server/ServerApp.kt
@@ -14,10 +14,6 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
 
-    install(CORS) {
-        host("localhost:8081")
-    }
-
     install(ContentNegotiation) {
         json()
     }
